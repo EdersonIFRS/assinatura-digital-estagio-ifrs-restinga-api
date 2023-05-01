740a759c380488fe3210069310c52d1d2d139099
EDERSONMEIRA <ederson.santos@softdesign.com.br>
2023-05-01 13:11:21 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifrs/restinga/assinaturadigitalestagioifrsrestingaapi/AssinaturaDigitalEstagioIfrsRestingaApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.restinga.assinaturadigitalestagioifrsrestingaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssinaturaDigitalEstagioIfrsRestingaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssinaturaDigitalEstagioIfrsRestingaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifrs/restinga/assinaturadigitalestagioifrsrestingaapi/AssinaturaDigitalEstagioIfrsRestingaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.restinga.assinaturadigitalestagioifrsrestingaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssinaturaDigitalEstagioIfrsRestingaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
