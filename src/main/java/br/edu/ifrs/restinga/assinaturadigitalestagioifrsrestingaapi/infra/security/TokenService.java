1f0702da9aaec2104aa4b5f2fd252783ccdd24a9
EDERSONMEIRA <ederson.santos@softdesign.com.br>
2023-05-09 02:28:07 -0300

    Implementação de Authenticação no sistema adicionado sugurança para os endpoints da api usando token JWT

--- src/main/java/br/edu/ifrs/restinga/assinaturadigitalestagioifrsrestingaapi/infra/security/TokenService.java
+++ src/main/java/br/edu/ifrs/restinga/assinaturadigitalestagioifrsrestingaapi/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import br.edu.ifrs.restinga.assinaturadigitalestagioifrsrestingaapi.model.Usuari
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,6 @@ public class TokenService {
     private String secret;
 
     public String gerarToken(Usuario usuario){
-        System.out.println(secret);
         try {
             Algorithm algoritimo = Algorithm.HMAC256(secret);
             return JWT.create()
@@ -30,6 +30,21 @@ public class TokenService {
         } catch (JWTCreationException exception){
             // Invalid Signing configuration / Couldn't convert Claims.
             throw  new RuntimeException("erro ao gerar token jwt",exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritimo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritimo)
+                    // specify an specific claim validations
+                    .withIssuer("API ASSINATURA.EST.IFRS")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            // Invalid signature/claims
+            throw  new RuntimeException("Token JWT INVALIDO ou EXPIRADO",exception);
         }
     }
 
