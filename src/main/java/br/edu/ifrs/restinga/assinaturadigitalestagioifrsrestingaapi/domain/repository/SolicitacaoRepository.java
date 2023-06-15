38c9b006e91995d0a6790c1604a8a44c89a57aa0
vinivcs97 <56758615+vinivcs97@users.noreply.github.com>
2023-06-14 21:13:14 -0300

    Update SolicitacaoRepository.java

--- src/main/java/br/edu/ifrs/restinga/assinaturadigitalestagioifrsrestingaapi/domain/repository/SolicitacaoRepository.java
+++ src/main/java/br/edu/ifrs/restinga/assinaturadigitalestagioifrsrestingaapi/domain/repository/SolicitacaoRepository.java
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SolicitacaoRepository  extends JpaRepository<SolicitarEstagio,Long> {
     List<SolicitarEstagio> findByAluno(Aluno aluno);
+    List<SolicitarEstagio> findByServidor(Servidor servidor);
 }
