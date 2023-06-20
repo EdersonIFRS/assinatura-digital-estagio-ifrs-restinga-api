aed967697178bd248bca7defdfdff441d5590f87
Maycon Padilha <maycon.jordan5000@gmail.com>
2023-06-19 23:12:28 -0300

    ajuste

---
+++ src/main/java/br/edu/ifrs/restinga/assinaturadigitalestagioifrsrestingaapi/dto/DadosVaga.java
@@ -0,0 +1,26 @@
+package br.edu.ifrs.restinga.assinaturadigitalestagioifrsrestingaapi.dto;
+
+import br.edu.ifrs.restinga.assinaturadigitalestagioifrsrestingaapi.model.Vaga;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosVaga (
+        @Nullable
+        long id,
+        @NotNull
+         String titulo,
+        @NotNull
+         String agencia,
+        @NotNull
+         String descricao,
+        @NotNull
+         String local,
+        String valor,
+        @NotNull
+        String turno
+){
+
+    public DadosVaga(Vaga vaga) {
+       this(vaga.getId(), vaga.getTitulo(), vaga.getAgencia(), vaga.getDescricao(), vaga.getLocal(), vaga.getValor(), vaga.getTurno());
+    }
+}
---
+++ src/main/java/br/edu/ifrs/restinga/assinaturadigitalestagioifrsrestingaapi/model/Vaga.java
@@ -0,0 +1,45 @@
+package br.edu.ifrs.restinga.assinaturadigitalestagioifrsrestingaapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "vagas")
+@Data
+@NoArgsConstructor
+
+public class Vaga {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String titulo;
+    @NotNull
+    private String agencia;
+    @NotNull
+    private String descricao;
+    @NotNull
+    private String local;
+    @NotNull
+    private String valor;
+    @NotNull
+    private String turno;
+
+
+    public Vaga(String titulo, String agencia, String descricao, String local, String valor, String turno) {
+        this.titulo = titulo;
+        this.agencia = agencia;
+        this.descricao = descricao;
+        this.local = local;
+        this.valor = valor;
+        this.turno = turno;
+    }
+
+}
